0f65c0014503b18437ef997e119884272bdf1ed5
EliasChen <eliaschen.dev@gmail.com>
2024-09-25 12:04:30 +0800

    add comment

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,7 @@ namespace _1_3
 {
     public partial class Form1 : Form
     {
-        public const float PI = 3.14f;
+        public const float PI = 3.14f; //宣告圓周率
         public Form1()
         {
             InitializeComponent();
@@ -25,39 +25,41 @@ namespace _1_3
 
         private void calc_click(object sender, EventArgs e)
         {
-            double radius = double.Parse(input.Text); //convert input text from String to double
-            double diameterRes = radius * 2;
-            double perimeterRes = diameterRes * PI;
-            double areaRes = Math.Pow(radius, 2) * PI;
+            double radius = double.Parse(input.Text); //宣告輸入半徑
+            double diameterRes = radius * 2; //直徑
+            double perimeterRes = diameterRes * PI; //周長
+            double areaRes = Math.Pow(radius, 2) * PI; //圓面積
             
+            //// 顯示計算結果
             diameter.Text = diameterRes.ToString();
             perimeter.Text = perimeterRes.ToString();
             area.Text = areaRes.ToString();
+            ////
 
            
         }
 
         private void plusCalc_Click(object sender, EventArgs e)
         {
-            double plusRes = double.Parse(plusA.Text) + double.Parse(plusB.Text);
+            double plusRes = double.Parse(plusA.Text) + double.Parse(plusB.Text); //加法
             plusResult.Text = plusRes.ToString();
         }
 
         private void minusCalc_Click(object sender, EventArgs e)
         {
-            double minusRes = double.Parse(minusA.Text) - double.Parse(minusB.Text);
+            double minusRes = double.Parse(minusA.Text) - double.Parse(minusB.Text); //減法
             minusResult.Text = minusRes.ToString();
         }
 
         private void multiplyCalc_Click(object sender, EventArgs e)
         {
-            double multiplyRes = double.Parse(multiplyA.Text) * double.Parse(multiplyB.Text);
-            multiplyResult.Text = multiplyRes.ToString();
+            double multiplyRes = double.Parse(multiplyA.Text) * double.Parse(multiplyB.Text); //乘法
+            multiplyResult.Text = multiplyRes.ToString(); 
         }
 
         private void divisionCalc_Click(object sender, EventArgs e)
         {
-            double divisionRes = double.Parse(dividedA.Text) / double.Parse(dividedB.Text);
+            double divisionRes = double.Parse(dividedA.Text) / double.Parse(dividedB.Text); //除法
             divisionResult.Text = divisionRes.ToString();
         }
     }
