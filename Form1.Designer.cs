0b6dd1ee19f38ef266610dfd6ee4037720168e6f
EliasChen <eliaschen.dev@gmail.com>
2024-09-25 10:09:12 +0800

    UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.text1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button1.Location = new System.Drawing.Point(291, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.textBox1.Location = new System.Drawing.Point(107, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // text1
+            // 
+            this.text1.AutoSize = true;
+            this.text1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.text1.Location = new System.Drawing.Point(32, 33);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(69, 20);
+            this.text1.TabIndex = 2;
+            this.text1.Text = "圓半徑";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label2.Location = new System.Drawing.Point(32, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "圓周長";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label3.Location = new System.Drawing.Point(107, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(2, 22);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label4.Location = new System.Drawing.Point(32, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "圓直徑";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label6.Location = new System.Drawing.Point(32, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "圓面積";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label1.Location = new System.Drawing.Point(107, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2, 22);
+            this.label1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label5.Location = new System.Drawing.Point(107, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(2, 22);
+            this.label5.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(418, 223);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label text1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
